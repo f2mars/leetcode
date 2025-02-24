64c94b89f51b0eda3cf713989d130d9e1cc885a8
f2mars <m.norkus01@gmail.com>
2025-02-24 14:16:21 +0200

    933 solution

--- src/933.NumberOfRecentCalls/script.ts
+++ src/933.NumberOfRecentCalls/script.ts
@@ -1,12 +1,18 @@
 // https://leetcode.com/problems/number-of-recent-calls/description/?envType=study-plan-v2&envId=leetcode-75
 
+// Complexity: Time O(1), Space O(N)
 class RecentCounter {
+    calls: number[];
+
     constructor() {
-        
+        this.calls = [];
     }
 
     ping(t: number): number {
-        return t;
+        this.calls.push(t);
+        const rescentStart = t - 3000;
+        const oldestRescent = this.calls.find(call => call >= rescentStart) ?? t;
+        return this.calls.length - this.calls.indexOf(oldestRescent);
     }
 }
 
