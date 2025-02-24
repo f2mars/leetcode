b8d020caa4d2d77644ddf28e9db67355a1ef0bc5
f2mars <m.norkus01@gmail.com>
2025-02-24 16:59:18 +0200

    more tests

--- src/649.DotaSenate/script.test.ts
+++ src/649.DotaSenate/script.test.ts
@@ -1,9 +1,33 @@
 import { predictPartyVictory } from "./script.js";
 
+test(`predictPartyVictory("R") should return "Radiant"`, () => {
+    expect(predictPartyVictory("R")).toBe("Radiant");
+});
+
+test(`predictPartyVictory("D") should return "Dire"`, () => {
+    expect(predictPartyVictory("D")).toBe("Dire");
+});
+
+test(`predictPartyVictory("DDRR") should return "Dire"`, () => {
+    expect(predictPartyVictory("DDRR")).toBe("Dire");
+});
+
 test(`predictPartyVictory("RD") should return "Radiant"`, () => {
     expect(predictPartyVictory("RD")).toBe("Radiant");
 });
 
 test(`predictPartyVictory("RDD") should return "Dire"`, () => {
     expect(predictPartyVictory("RDD")).toBe("Dire");
+});
+
+test(`predictPartyVictory("RDDRRD") should return "Radiant"`, () => {
+    expect(predictPartyVictory("RDDRRD")).toBe("Radiant");
+});
+
+test(`predictPartyVictory("RDDRRDD") should return "Radiant"`, () => {
+    expect(predictPartyVictory("RDDRRDD")).toBe("Radiant");
+});
+
+test(`predictPartyVictory("RDDRRDD") should return "Dire"`, () => {
+    expect(predictPartyVictory("RDDRRDD")).toBe("Dire");
 });
