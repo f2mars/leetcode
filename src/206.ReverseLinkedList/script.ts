b18d7e2507a776d1d43adf1852a96e56b55744cc
f2mars <m.norkus01@gmail.com>
2025-03-03 10:01:00 +0200

    More tests

--- src/206.ReverseLinkedList/script.ts
+++ src/206.ReverseLinkedList/script.ts
@@ -1,4 +1,9 @@
 // https://leetcode.com/problems/reverse-linked-list/description/?envType=study-plan-v2&envId=leetcode-75
+/* 
+Constraints:
+- The number of nodes in the list is the range [0, 5000].
+- -5000 <= Node.val <= 5000
+*/
 
 class ListNode {
     val: number
