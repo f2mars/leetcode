134d7e147e4811de9a7f9effd66199ae6b291c73
f2mars <79808447+f2mars@users.noreply.github.com>
2025-03-04 11:09:14 +0200

    More tests

--- src/2130.MaximumTwinSumLinkedList/script.test.ts
+++ src/2130.MaximumTwinSumLinkedList/script.test.ts
@@ -1,8 +1,17 @@
 import { pairSum, ListNode } from "./script.js";
 
+test("head = [1,100000] should return 100001", () => {
+    expect(pairSum(new ListNode(1, new ListNode(100000, null)))).toBe(100001);
+});
+
 test("head = [5,4,2,1] should return 6", () => {
     expect(pairSum(new ListNode(5, new ListNode(4, new ListNode(2, new ListNode(1, null)))))).toBe(6);
 });
 
-//head = [4,2,2,3] to be 7
-// head = [1,100000] to be 100001
+test("head = [4,2,2,3] should return 7", () => {
+    expect(pairSum(new ListNode(4, new ListNode(2, new ListNode(2, new ListNode(3, null)))))).toBe(7);
+});
+
+test("head = [4,2,7,1,2,3] should return 8", () => {
+    expect(pairSum(new ListNode(4, new ListNode(2, new ListNode(7, new ListNode(1, new ListNode(2, new ListNode(3, null)))))))).toBe(8);
+});
