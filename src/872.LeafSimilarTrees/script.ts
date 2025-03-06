018e5c3d45f36f8604fa6f1c4e469b5a7eb0e2b9
f2mars <m.norkus01@gmail.com>
2025-03-06 13:41:20 +0200

    Intuitive solution: 2 arrays and recursion

--- src/872.LeafSimilarTrees/script.ts
+++ src/872.LeafSimilarTrees/script.ts
@@ -18,7 +18,32 @@ class TreeNode {
     }
 }
 
+// Intuition: Collect leaft in separete arrays using recursion
+// Complexity: Time O(N), Space O(N)
 function leafSimilar(root1: TreeNode | null, root2: TreeNode | null): boolean {
+    const collectLeafs = (tree: TreeNode | null, storage: number[]) => {
+        if (tree === null) return;
+        if (tree.left === null && tree.right === null) {
+            storage.push(tree.val);
+            return;
+        } 
+
+        collectLeafs(tree.left, storage);
+        collectLeafs(tree.right, storage); 
+    }
+
+    const leafs1: number[] = [];
+    const leafs2: number[] = [];
+
+    collectLeafs(root1, leafs1);
+    collectLeafs(root2, leafs2);
+
+    if (leafs1.length !== leafs2.length) return false;
+
+    for (let i = 0; i < leafs1.length; i++) {
+        if (leafs1[i] !== leafs2[i]) return false;
+    }
+
     return true;
 };
 
