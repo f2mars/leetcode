ac3d00d8250ce8f849a8a5c6c39548cc5bc66e74
f2mars <79808447+f2mars@users.noreply.github.com>
2025-03-21 16:32:56 +0200

    Stack solution

--- src/94.BinaryTreeInorderTraversal/script.ts
+++ src/94.BinaryTreeInorderTraversal/script.ts
@@ -16,12 +16,35 @@ class TreeNode {
         this.right = (right === undefined ? null : right)
     }
 }
-
+ 
 // Recursive
 // Complexity: Time O(N), Space O(N)
 function inorderTraversal(root: TreeNode | null): number[] {
     if (root === null) return [];
     return [...inorderTraversal(root.left), root.val, ...inorderTraversal(root.right)];
 };
+
+/* 
+// Stack
+// Complexity: Time O(N), Space O(N)
+function inorderTraversal(root: TreeNode | null): number[] {
+    const result: number[] = [];
+    const stack: TreeNode[] = [];
+    let currentNode = root;
+
+    while (currentNode !== null || stack.length !== 0) {
+        while (currentNode !== null) {
+            stack.push(currentNode);
+            currentNode = currentNode.left;
+        }
+
+        currentNode = stack.pop()!;
+        result.push(currentNode.val);
+        currentNode = currentNode.right;
+    }
+
+    return result;
+};
+ */
 
 export { TreeNode, inorderTraversal }
